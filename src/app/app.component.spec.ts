839b8b17c9d30b92fe2c96c7d9fd70458cbb17ba
Yash <yasodha@rjtcompuquest.com>
2018-11-05 14:52:19 -0600

    Add creditcard directive

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,3 +15,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
  });
+
+
